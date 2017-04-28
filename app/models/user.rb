e59c09bd60a0c647bb221dcf043646e57c4b3739
Matthew Fischer <email": null>
2017-04-28 15:21:39 -0700

    Successfully test attributes of user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
 
   has_and_belongs_to_many :groups
   has_many :hangouts, through: :groups, source: :hangout
-  has_many :created_groups, foreign_key: :admin_id
+  has_many :created_groups, class_name: "Group", foreign_key: "admin_id"
   has_many :created_hangouts, foreign_key: :creator_id
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    let (:admin){User.create(name: "Admin", email: "admin@admin.com", password: "password")}
+    let (:creator){User.create(name: "Creator", email: "creator@creator.com", password: "password")}
+    let (:member){User.create(name: "Member", email: "member@member.com", password: "password")}
+    let (:group){Group.new(name: "Test")}
+
+    before(:each) do
+      admin.created_groups << group
+      group.save
+      group.users << creator
+      group.users << member
+    end
+
+    context "User" do
+      it "has a name" do
+        expect(admin.name).to eq "Admin"
+      end
+
+      it "has an email" do
+        expect(admin.email).to eq "admin@admin.com"
+      end
+
+      it "has a password" do
+        expect(admin.password).not_to eq nil
+      end
+    end
+  end
 end
