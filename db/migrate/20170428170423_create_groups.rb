cfe0a7074a0c5e88c4c55a565f0e66cf185c2eb5
Matthew Fischer <email": null>
2017-04-28 10:18:53 -0700

    Write migrations for models

--- db/migrate/20170428170423_create_groups.rb
+++ db/migrate/20170428170423_create_groups.rb
@@ -1,8 +1,10 @@
 class CreateGroups < ActiveRecord::Migration[5.0]
   def change
     create_table :groups do |t|
+      t.string :name
+      t.integer :admin_id
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20170428170430_create_hangouts.rb
+++ db/migrate/20170428170430_create_hangouts.rb
@@ -1,8 +1,10 @@
 class CreateHangouts < ActiveRecord::Migration[5.0]
   def change
     create_table :hangouts do |t|
+      t.integer :creator_id
+      t.integer :group_id
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
