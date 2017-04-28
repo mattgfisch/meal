732c4a06101afa0e8ab1d4b0719dff14f1002f8a
Matthew Fischer <email": null>
2017-04-28 15:32:34 -0700

    Write tests for members and creators

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_secure_password
 
   has_and_belongs_to_many :groups
-  has_many :hangouts, through: :groups, source: :hangout
+  has_many :hangouts, through: :groups, source: :hangouts
   has_many :created_groups, class_name: "Group", foreign_key: "admin_id"
-  has_many :created_hangouts, foreign_key: :creator_id
+  has_many :created_hangouts, class_name: "Hangout", foreign_key: "creator_id"
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,12 +6,18 @@ RSpec.describe User, type: :model do
     let (:creator){User.create(name: "Creator", email: "creator@creator.com", password: "password")}
     let (:member){User.create(name: "Member", email: "member@member.com", password: "password")}
     let (:group){Group.new(name: "Test")}
+    let (:hangout){Hangout.new()}
 
     before(:each) do
       admin.created_groups << group
       group.save
+      group.users << admin
       group.users << creator
       group.users << member
+
+      creator.created_hangouts << hangout
+      group.hangouts << hangout
+      hangout.save
     end
 
     context "User" do
@@ -27,5 +33,28 @@ RSpec.describe User, type: :model do
         expect(admin.password).not_to eq nil
       end
     end
+
+    context "Admin" do
+      it "has a created group" do
+        expect(admin.created_groups[0].name).to eq "Test"
+      end
+
+      it "is a member of created group" do
+        expect(admin.groups[0].name).to eq "Test"
+      end
+    end
+
+    context "Member" do
+      it "is a member of a group" do
+        expect(member.groups[0].name).to eq "Test"
+      end
+    end
+
+    context "Creator" do
+      it "has a created hangout" do
+        expect(creator.hangouts[0]).to be_a Hangout
+      end
+    end
+
   end
 end
