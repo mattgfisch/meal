a2b33bd4490161e295504a77f1ac253f7729c6f5
Matthew Fischer <email": null>
2017-04-28 16:20:23 -0700

    Finish test for location

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,3 @@
 class Location < ApplicationRecord
-  belongs_to :hangouts
+  belongs_to :hangout
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,4 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Location, type: :model do
+  describe "Location" do
+    let (:hangout){Hangout.new()}
+    let (:loc1){Location.create(latitude: 11.33242345, longitude: 12.2343234)}
+    let (:loc2){Location.create(latitude: 12.3324234, longitude: 12.2343234)}
+    before(:each) do
+      hangout.locations << loc1
+      hangout.locations << loc2
+      hangout.save
+    end
+
+    context "attributes" do
+      it "has a longitude" do
+        expect(loc1.longitude).to eq 12.2343234
+      end
+
+      it "has a latitude" do
+        expect(loc1.latitude).to eq 11.33242345
+      end
+    end
+
+    context "associations" do
+      it "has a hangout" do
+        expect(loc2.hangout).to be_a Hangout
+      end
+    end
+
+  end
 end
