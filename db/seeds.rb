41a3a7a38f41300f61569a5b400a329b9c3f94bf
Matthew Fischer <email": null>
2017-04-29 23:29:48 -0700

    Include last pieces of users-show-page branch

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(name: "Matt", email: "matt@matt.com", password: "password")
-User.create(name: "Dillon", email: "dillon@dillon.com", password: "password")
+User.create(name: "Eitan", email: "eitan@eitan.com", password: "password")
+User.create(name: "Austin", email: "austin@austin.com", password: "password")
+
+matt = User.create!(name: "Matt", email: "matt@matt.com", password: "password")
+dillon = User.create!(name: "Dillon", email: "dillon@dillon.com", password: "password")
+group1 = Group.create!(name: "Test", admin_id: 1)
+hangout1 = Hangout.create!(creator_id: 2, group_id: 1)
+
+group1.members << dillon
+group1.members << matt
+hangout1.members << matt
+hangout1.members << dillon
