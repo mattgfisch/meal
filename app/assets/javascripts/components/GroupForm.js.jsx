33d3837f607dadc34cd583f79e33f1aefaa2ce98
Eitan Greene <email": null>
2017-04-29 23:55:01 -0700

    Adds group creation form

--- app/assets/javascripts/components/GroupForm.js.jsx
+++ app/assets/javascripts/components/GroupForm.js.jsx
@@ -5,7 +5,30 @@ class GroupCreationForm extends React.Component {
 
   render () {
     return (
-      <h2>CREATE GROUP</h2>
+      <div className='card'>
+        <div className='card-header'>
+          <h2>New Group</h2>
+        </div>
+        <form action='/users' method='post'>
+          <div className='form-group'>
+            <label htmlFor='name'>Name</label>
+            <input type='text' ref='groupName' className='form-control' id='name' placeholder='John Doe' />
+          </div>
+          <div className='form-group'>
+            <label htmlFor='exampleInputPassword1'>Send invites to:</label>
+            <input type='email' className='form-control' placeholder='johndoe@email.com' />
+          </div>
+          <div className='form-group'>
+            <input type='email' className='form-control' placeholder='johndoe@email.com' />
+          </div>
+          <div className='form-group'>
+            <input type='email' className='form-control' placeholder='johndoe@email.com' />
+          </div>
+          <div className='register-btn'>
+            <button type='submit' className='btn btn-default'>Invite Users</button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
