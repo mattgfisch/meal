447676081624555b912c69a1ffe23f67c6ccec7d
Dillon Estelle & Eitan Greene <pair+destelle.eitan-g@devbootcamp.com>
2017-05-02 12:14:39 -0700

    add test to group controller

--- spec/controllers/groups_controller_spec.rb
+++ spec/controllers/groups_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe GroupsController, type: :controller do
+  describe 'post group add members' do
+    let!(:user) { User.create!(name: 'Josh', email: 'email@email.com', password: 'password') }
+    let!(:group) { Group.create!(name: 'dog', admin: user) }
+    let!(:user2) { User.create!(name: 'austin', email: 'austin@austin.com', password: 'password') }
 
+
+    it 'responds with status code 200' do
+      get :add_members, params: {id: group.id, currentEmail: "austin@austin.com"}
+      expect(response).to have_http_status 200
+    end
+  end
 end
