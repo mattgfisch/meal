f81f5b281c88261d5d69bc8b37f71b6ec0eee5f6
Dillon Estelle & Eitan Greene <pair+destelle.eitan-g@devbootcamp.com>
2017-05-02 10:17:28 -0700

    tests for admin form

---
+++ spec/features/admin_show_form.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe 'GroupShow' do
+  feature 'Admin form on group show', js: true do
+    let!(:user) { User.create!(name: 'Matt', email: 'matt@matt.com', password: 'password') }
+    let!(:new_user) { User.create!(name: 'Jack', email: 'jack@jack.com', password: 'password') }
+    let!(:firstgroup) { Group.create!(name: 'to Delete', admin_id: new_user.id) }
+    let!(:group) { Group.create!(name: 'Test', admin_id: user.id) }
+    let!(:group2) { Group.create!(name: '1', admin_id: user.id) }
+    let!(:group3) { Group.create!(name: '2', admin_id: user.id) }
+    let!(:group4) { Group.create(name: '3', admin_id: new_user.id) }
+    let!(:hangout) {Hangout.create!(creator_id: user.id, group_id: group.id)}
+
+    before(:each) do
+      firstgroup.members << user
+      group.members << user
+      group2.members << user
+      group3.members << user
+      group4.members << user
+      hangout.members << user
+      visit '/'
+      fill_in 'Email', with: 'matt@matt.com'
+      fill_in 'Password', with: 'password'
+      click_on 'Log In'
+    end
+
+    it 'Can click on created groups and see add users because we are group admin' do
+      click_link('created', :match => :first)
+      expect(page).to have_content('Add Users')
+    end
+
+    it 'Can click on joined groups and to not have admin features' do
+      click_link('joined', :match => :first)
+      expect(page).not_to have_content('Add Users')
+    end
+
+  end
+end
