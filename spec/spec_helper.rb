1174b5a26130d29085ce39662a4241095e8894c9
Matthew Fischer <email": null>
2017-05-02 15:28:30 -0700

    Remove wait_for_ajax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -21,7 +21,6 @@ Coveralls.wear!('rails')
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
-  config.include WaitForAjax, type: :feature
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
