0c717cd0f80b051b0eecbfca1d3ebc6e8e7f42a5
Matthew Fischer <email": null>
2017-05-02 15:31:19 -0700

    Remove Current Hangouts

--- app/assets/javascripts/components/UserContent.js.jsx
+++ app/assets/javascripts/components/UserContent.js.jsx
@@ -7,7 +7,6 @@ class UserContent extends React.Component {
           <h2>Your Meals</h2>
         </div>
         <div className='card-block'>
-          <UserHangoutContent changeStates={this.props.changeStates} />
           <UserGroupContent  changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
           <AdminGroupContent changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
         </div>
