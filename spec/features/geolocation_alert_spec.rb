dad6a4eecc1c664896281a079b4229d3034c8e08
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-05-03 15:58:33 -0700

    Tests warning message for unavailable geolocation services

--- spec/features/geolocation_alert_spec.rb
+++ spec/features/geolocation_alert_spec.rb
@@ -4,24 +4,20 @@ describe 'Showing restaurants', js: true do
   let!(:user) { User.create!(name: 'Matt', email: 'matt@matt.com', password: 'password') }
   let!(:group1) { Group.create!(name: 'Grouptastic', admin_id: user.id) }
 
-  before(:each) do
-    group1.members << user
-    visit '/'
-    fill_in 'Email', with: user.email
-    fill_in 'Password', with: 'password'
-    click_on 'Log In'
-    button = page.find('.joined-link', match: :first)
-    button.click
-  end
+  feature 'Display warning' do
+    it "Displays if location services are unavailable" do
+      group1.members << user
 
-  feature 'Create hangout' do
-    it "shows user is hanging out" do
-      click_on 'Create Hangout'
-      
       visit '/'
+      fill_in 'Email', with: user.email
+      fill_in 'Password', with: 'password'
+      click_on 'Log In'
+
       button = page.find('.joined-link', match: :first)
       button.click
-      expect(page).to have_content 'HANGING OUT'
+      click_on 'Create Hangout'
+
+      expect(page).to have_content 'Warning'
     end
   end
 end
