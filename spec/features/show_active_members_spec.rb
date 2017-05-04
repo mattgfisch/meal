02357c4bb6e01c03546e39c6325f0ed961c28a34
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-05-03 20:25:43 -0700

    Add sleep

--- spec/features/show_active_members_spec.rb
+++ spec/features/show_active_members_spec.rb
@@ -13,8 +13,9 @@ describe 'ShowActiveMembers' do
       hangout.members << user
 
       visit '/'
-      fill_in 'email', with: 'matt@matt.com'
-      fill_in 'password', with: 'password'
+      sleep 2
+      fill_in 'Email', with: 'matt@matt.com'
+      fill_in 'Password', with: 'password'
       click_on 'Log In'
 
       click_link('Test', match: :first)
