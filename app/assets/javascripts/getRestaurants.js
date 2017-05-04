22656f2db01d808fcf5e316a5a77e5662807f750
Eitan Greene & Matthew Fischer <pair+eitan-g.mattgfisch@devbootcamp.com>
2017-05-03 21:21:15 -0700

    Create variable to store restaurant name

--- app/assets/javascripts/getRestaurants.js
+++ app/assets/javascripts/getRestaurants.js
@@ -18,7 +18,7 @@ function callback (results, status) {
   if (status === google.maps.places.PlacesServiceStatus.OK) {
     var collection = '<div><h3>Restaurants</h3></div>'
     for (var i = 0; i < results.length; i++) {
-      let restaurantName
+      let restaurantName = results[i].name
       let url = 'https://www.google.com/maps/place/' + restaurantName.replace(/\s/g, '+')
       collection += ('<p>' + '<a href=' + url + '>' + restaurantName + '</p>')
     }
