42682d58e0d0f1998a59c1ea00792769623005f1
Dillon Estelle & Eitan Greene <pair+destelle.eitan-g@devbootcamp.com>
2017-05-04 16:38:42 -0700

    Refactors tests to work with new dropdown menu

--- spec/features/admin_delete_hangout_spec.rb
+++ spec/features/admin_delete_hangout_spec.rb
@@ -6,6 +6,10 @@ describe 'GroupShow' do
     let!(:creator) { User.create!(name: 'Max', email: 'max@max.com', password: 'password')}
     let!(:group) { Group.create!(name: "Test", admin_id: user.id) }
 
+    def find(text)
+      page.find("a", :text => /\A#{text}\z/)
+    end
+
     before(:each) do
       group.members << creator
       group.members << user
@@ -16,27 +20,31 @@ describe 'GroupShow' do
       click_on 'Log In'
 
       click_link('Test', match: :first)
-      click_on 'Create Hangout'
+      click_on 'Options'
+      find('Create Hangout').click
+      sleep 6
+      click_on 'Options'
+
     end
 
     it "Hangout admin page shows delete hangout button" do
-      sleep 5
       expect(page).to have_content('Delete Hangout')
     end
 
     it "Hangout admin can delete hangout" do
-      sleep 5
-      click_on 'Delete Hangout'
+      find('Delete Hangout').click
       expect(page).not_to have_content('Restaurants')
     end
 
     it "removes user from hangout and button goes back to join hangout" do
-      click_on 'Delete Hangout'
+      find('Delete Hangout').click
+      click_on 'Options'
       expect(page).to have_content('Create Hangout')
     end
 
     it "removes user from hangout and icon next to name is removed" do
-      click_on 'Delete Hangout'
+      find('Delete Hangout').click
+
       user_div = page.find("div", :text => /\AMatt\z/)
       expect(user_div).not_to have_selector("button")
     end
