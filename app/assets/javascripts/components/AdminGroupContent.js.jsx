78c6dfd9beb763db133b59cbb0b2041e229aac91
Matthew Fischer <email": null>
2017-05-04 21:39:54 -0700

    Center create group button

--- app/assets/javascripts/components/AdminGroupContent.js.jsx
+++ app/assets/javascripts/components/AdminGroupContent.js.jsx
@@ -1,29 +1,19 @@
 class AdminGroupContent extends React.Component {
   render () {
     return (
-      <div className='card user-content'>
-        <div className='card-header created'>
-<<<<<<< HEAD
-          <div  id='created-groups'>
+      <div className='card user-content created'>
+        <div className='card-header'>
+          <div id='created-groups'>
             <h3>
-                Created Groups
-            </h3>
-            <UserCreateGroup id='create-button' changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
-          </div>
-          <div className='card-block user-home'>
-            <AdminGroupList changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
-          </div>
-=======
-          <h3>
               Created Groups
-          </h3>
-          <div id='create-btn'>
-            <UserCreateGroup changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
+            </h3>
+            <div id='create-btn' className='create-group-btn'>
+              <UserCreateGroup changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
+            </div>
           </div>
         </div>
         <div className='card-block user-home'>
           <AdminGroupList changeStates={this.props.changeStates} sessionID={this.props.sessionID} username={this.props.username} />
->>>>>>> master
         </div>
       </div>
     )
